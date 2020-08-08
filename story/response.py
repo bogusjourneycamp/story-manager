d2d2726fb6923720c60bd8d4f063b3c0b2fc5c2f
Hanu Kunduru <hanu@openinvest.co>
2020-08-08 13:58:27 -0700

    revert: change Access-Control-Allow-Origin

--- story/response.py
+++ story/response.py
@@ -7,7 +7,7 @@ def response(message, status_code):
         "body": json.dumps(message),
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "https://nervous-blackwell-595d82.netlify.app",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": "true",
             "Access-Control-Allow-Methods": "GET,OPTIONS,POST",
         },
