dc5d17d116508ea9e987a76e10b88af9dbb8cf6e
Hanu Kunduru <hanu@openinvest.co>
2020-08-08 13:49:51 -0700

    feat: add Access-Control-Allow-Headers

--- story/response.py
+++ story/response.py
@@ -7,8 +7,9 @@ def response(message, status_code):
         "body": json.dumps(message),
         "headers": {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": "true",
-            "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+            "Access-Control-Allow-Methods": "GET,OPTIONS,POST",
         },
     }
