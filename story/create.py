86d62f826a967cf86add7e2d2f2e83386a9b3e55
Hanu Kunduru <hanu@openinvest.co>
2020-08-08 13:12:00 -0700

    chore: refactor and abstract response code

--- story/create.py
+++ story/create.py
@@ -1,32 +1,18 @@
 import boto3
-import json
 from story.tree_validator import TreeValidator
 
 def create(event, context):
     if "body" not in event:
-        return {
-            "statusCode": 400,
-            "body": json.dumps("No body passed in event")
-        }
-    
+        return response("No body passed in event", 400)
+
     tree = json.loads(event["body"])
     
     (is_valid, reason) = TreeValidator().check_tree_validity(tree)
     
     if not is_valid:
-        return {
-            "statusCode": 400,
-            "body": json.dumps(f"Invalid body passed. Reason: {reason}")
-        }
+        return response(f"Invalid body passed. Reason: {reason}", 400)
 
     story_table = boto3.resource('dynamodb').Table('story-manager-dev')
     story_table.put_item(Item=tree)
     
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "https://nervous-blackwell-595d82.netlify.app",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        }
-    }
+    return response("", 200)
---
+++ story/response.py
@@ -0,0 +1,12 @@
+import json
+
+
+def response(message, status_code):
+    return {
+        'statusCode': str(status_code),
+        'body': json.dumps(message),
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+    }
