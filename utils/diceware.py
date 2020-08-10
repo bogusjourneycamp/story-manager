29855a9b154727a7c0df694945c179d65585bcca
Hanu Kunduru <hanu@openinvest.co>
2020-08-09 19:29:54 -0700

    refactor: simplify passphrase generation code

--- utils/diceware.py
+++ utils/diceware.py
@@ -5,15 +5,12 @@ from six.moves import range
 from utils.word_list import word_list
 
 
-def roll_dice(n=5):
-    """Rolls a number of dice (default: 5) n times"""
-    return "".join([str(random.SystemRandom().randint(1, 6)) for _ in range(5)])
-
-
 def generate_passphrase(word_count):
     rolls = []
     for i in range(word_count):
-        rolls.append(roll_dice())
+        #Use systemrandom for more accurate randomness and creates 5 dice rolls
+        dice_roll = "".join([str(random.SystemRandom().randint(1, 6)) for _ in range(5)])
+        rolls.append(dice_roll)
     # Loop through the list of rolls, looking each up in the wordlist and
     # adding the corresponding word to the passphrase.
     passphrase = ""
