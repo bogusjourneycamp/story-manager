45e91200840a2acb64d05b90e757b39b3bac51c5
Scott Snelgrove <scott.snelgrove1@gmail.com>
2020-08-23 20:51:35 -0700

    changed email message

--- story_manager/report.py
+++ story_manager/report.py
@@ -6,33 +6,13 @@ from botocore.exceptions import ClientError
 from utils.location_validator import LocationValidator
 from utils.response import response
 
-# SENDER = "charliesummers@gmail.com"
-# RECIPIENT = "charliesummers@gmail.com"
-SENDER = "scott.snelgrove1@gmail.com"
-RECIPIENT = "scott.snelgrove1@gmail.com"
-AWS_REGION = "us-west-2"
-SUBJECT = "Amazon SES Test"
-BODY_TEXT = ("This is a test")
-CHARSET = "UTF-8"
-BODY_HTML = """<html>
-<head></head>
-<body>
-  <h1>Amazon SES Test (SDK for Python)</h1>
-  <p>This email was sent with
-    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    <a href='https://aws.amazon.com/sdk-for-python/'>
-      AWS SDK for Python (Boto)</a>.</p>
-</body>
-</html>
-            """            
-
 def report(event, context):
     print("Event", event)
 
-    # if "body" not in event:
-    #     return response("No body passed in event", 400)
+    if "body" not in event:
+        return response("No body passed in event", 400)
 
-    # report_info = json.loads(event["body"])
+    report_info = json.loads(event["body"])
 
     if "pathParameters" not in event:
         return response("No location found in pathParameters", 400)
@@ -49,6 +29,22 @@ def report(event, context):
         )
     
     client = boto3.client('ses')
+    # SENDER = "charliesummers@gmail.com"
+    # RECIPIENT = "charliesummers@gmail.com"
+    SENDER = "scott.snelgrove1@gmail.com"
+    RECIPIENT = "scott.snelgrove1@gmail.com"
+    # AWS_REGION = "us-west-2"
+    SUBJECT = "Location %s Reported" % (location)
+    CHARSET = "UTF-8"
+    BODY_HTML = """
+    <html>
+    <head></head>
+    <body>
+        <h1>Report for location %s</h1>
+        <p>%s</p>
+    </body>
+    </html>
+                """ % (location, json.dumps(report_info, sort_keys=True, indent=4, separators=(',',': ')))
     try:
         response = client.send_email(
             Destination={
@@ -62,10 +58,10 @@ def report(event, context):
                         'Charset': CHARSET,
                         'Data': BODY_HTML,
                     },
-                    'Text': {
-                        'Charset': CHARSET,
-                        'Data': BODY_TEXT,
-                    },
+                    # 'Text': {
+                    #     'Charset': CHARSET,
+                    #     'Data': BODY_TEXT,
+                    # },
                 },
                 'Subject': {
                     'Charset': CHARSET,
